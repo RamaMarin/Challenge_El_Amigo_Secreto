3a12ffa720eb14b0018d79289b7452c583d81c21
Rama Marin <ramamarin2013@gmail.com>
2025-08-20 12:34:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function agregarAmigo() {
     if (nombreAmigo.trim() !== '') {
         amigos.push(nombreAmigo.trim());
         mostrarAmigos();
-        document.getElementById('amigo').value = ''; // Limpia el campo de entrada
+        document.getElementById('amigo').value = ''; 
     } else {
         alert("Por favor, ingresa un nombre válido.");
     }
@@ -16,7 +16,7 @@ function agregarAmigo() {
 // Función para mostrar la lista de amigos en el HTML
 function mostrarAmigos() {
     let listaHTML = document.getElementById('listaAmigos');
-    listaHTML.innerHTML = ''; // Limpia la lista antes de volver a llenarla
+    listaHTML.innerHTML = ''; 
     amigos.forEach(amigo => {
         let li = document.createElement('li');
         li.textContent = amigo;
@@ -31,11 +31,10 @@ function sortearAmigo() {
         return;
     }
 
-    // Selecciona un índice aleatorio del array de amigos
     let indiceAleatorio = Math.floor(Math.random() * amigos.length);
     let amigoSorteado = amigos[indiceAleatorio];
 
-    // Muestra el resultado en el HTML
     let resultadoHTML = document.getElementById('resultado');
     resultadoHTML.innerHTML = `<h3>¡El amigo secreto sorteado es: ${amigoSorteado}!</h3>`;
+
 }
